d97e58b7a35393c0462a979ba055820fb1816ce7
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-10 21:40:56 -0500

    Video 112 Cargando Json con fetch API

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -1,4 +1,5 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 
 function cargarTXT() {
     fetch('datos.txt')
@@ -8,6 +9,28 @@ function cargarTXT() {
         .then(function(empleados) {
             console.log(empleados);
             document.getElementById('resultado').innerHTML = empleados;
+        })
+        .catch(function(error){
+            console.log(error);
+
+        });
+}
+
+function cargarJSON(){
+    fetch('empleados.json')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado) {
+                html += `
+                <li>${empleado.nombre} ${empleado.puesto}</li>
+                
+                `;
+                
+            })
+            document.getElementById('resultado').innerHTML = html;
         })
         .catch(function(error){
             console.log(error);
