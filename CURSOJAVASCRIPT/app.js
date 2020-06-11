818f4e6063e34fe41332b47d414615fb5a0d87b2
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-10 20:57:44 -0500

    Video 110 Promises

--- CURSOJAVASCRIPT/app.js
+++ CURSOJAVASCRIPT/app.js
@@ -1,5 +1,5 @@
 
-const paises = ['Londres', 'New York', 'Madrid', 'París', 'Viena'];
+/*const paises = ['Londres', 'New York', 'Madrid', 'París', 'Viena'];
 
 
 function nuevoPais(pais, colback){
@@ -24,3 +24,32 @@ function mostrarPaíses() {
 nuevoPais('Alemania', mostrarPaíses);
 
 mostrarPaíses();
+
+const esperando = new Promise(function(resolve, reject ) {
+    setTimeout(function() {
+        resolve('Se ejecuto');
+
+    }, 5000);
+
+});
+esperando.then(function(mensaje) {
+    console.log(mensaje);
+
+})*/
+
+const aplicarDescuento = new Promise(function(resolve, reject ) {
+    const descuento = false;
+    if(descuento) {
+        resolve('Descuento Aplicado');
+
+    }else {
+        reject('No se puede aplicar el descuento');
+    }
+
+});
+
+aplicarDescuento.then(function(resultado){
+    console.log(resultado);
+}).catch (function(error){
+    console.log(error);
+});
