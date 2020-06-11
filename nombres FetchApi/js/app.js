4b05c5d6ecd7b1e034c875292774ccea28715770
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-10 23:56:59 -0500

    Video 114 Cambio de Ajax a Fetch

--- nombres FetchApi/js/app.js
+++ nombres FetchApi/js/app.js
@@ -13,12 +13,13 @@ function cargarNombres(e) {
      const cantidad = document.getElementById('numero').value;
 
 
-
-     let url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${ origenSeleccionado }&limit=${ cantidad }&api_key=${ apiKey }&format=json`;
-
+     let url = '';
+     url += `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${ origenSeleccionado }&limit=${ cantidad }&api_key=${ apiKey }&format=json`;
+     
+     
      // Conectar con ajax
      // Iniciar XMLHTTPRequest
-     const xhr = new XMLHttpRequest();
+     /*const xhr = new XMLHttpRequest();
      // Abrimos la conexion
      xhr.open('GET', url, true);
      // Datos e impresion del template
@@ -45,6 +46,27 @@ function cargarNombres(e) {
           }
      }
      // Enviar el Request
-     xhr.send();
+     xhr.send();*/
 
+
+
+     //Crear Fetch
+     fetch(url)
+          .then(function(res){
+               return res.json();
+          })
+          .then(function(artistas){
+               let htmlNombres = `<h2>Top de Artistas</h2>`;
+               htmlNombres += `<ul class="lista">`;
+               artistas.topartists.artist.forEach(function(artista) {
+                    htmlNombres += `
+                         <li><a href='${ artista.url }' target="_blank">${artista.name}</a></li>
+                    `;
+               })
+               htmlNombres += `</ul>`;
+               document.querySelector('#resultado').innerHTML = htmlNombres;
+          })
+          .catch(function(error){
+               console.log(error);
+          })
 }
